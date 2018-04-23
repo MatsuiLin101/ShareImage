bf3707bc216b35eeb6e0694854fdffc886d04f06
coder <coder@YARE.local>
2018-04-23 13:48:50 +0800

    creata image table

--- shareimageapp/models.py
+++ shareimageapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    name = models.CharField(max_length=8, null=False)
+    img = models.ImageField(upload_to='shareimageapp/', null=False)
+
+    def __str__(self):
+        return self.name
